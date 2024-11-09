b738bf2e6ed7edf3d6ec0304e4d6766de0c3fe91
Porvah <3msabdullah@gmail.com>
2024-11-09 20:45:16 +0200

    fixing maven issues

---
+++ backend/sayes/src/main/java/com/utopia/Sayes/SayesApplication.java
@@ -0,0 +1,13 @@
+package com.utopia.Sayes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SayesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SayesApplication.class, args);
+	}
+
+}
