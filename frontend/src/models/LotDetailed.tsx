5c6e98cd252a708e5bdd560bdb295b0eb9e23c0f
Ayman Algamal <ayman01287985950@gmail.com>
2024-12-25 00:07:51 +0200

    Adding Spot interface.

---
+++ frontend/src/models/LotDetailed.tsx
@@ -0,0 +1,14 @@
+export interface LotDetailed {
+    lot_id: number;
+    manager_id: number;
+    longitude: number;
+    latitude: number;
+    revenue: number;
+    price: number;
+    num_of_spots: number;
+    lot_type: string;
+    penalty: number;
+    fee: number;
+    time: string;
+    details: any;
+}
