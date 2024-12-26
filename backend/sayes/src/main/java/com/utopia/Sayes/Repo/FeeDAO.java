d1cd4ad1b4f631a18bab0a427953256ce28f6438
RowanYoussef <150041546+RowanYoussef@users.noreply.github.com>
2024-12-26 21:09:37 +0200

    fee dao , table

---
+++ backend/sayes/src/main/java/com/utopia/Sayes/Repo/FeeDAO.java
@@ -0,0 +1,35 @@
+package com.utopia.Sayes.Repo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class FeeDAO {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+    public void addFee(long lot_id, long driverId , double fee){
+        String query = "INSERT INTO fees " +
+                " (lot_id, driver_id ,fee) VALUES (?, ? , ?)";
+        int rows =  jdbcTemplate.update(query,lot_id,driverId,fee);
+        if(rows == 0){
+            throw new RuntimeException("can't insert this fee");
+        }
+    }
+    public void deleteFee(long lot_id, long driverId){
+        String query = "DELETE FROM fees WHERE lot_id = ? AND driver_id = ?";
+        int rows =  jdbcTemplate.update(query,lot_id,driverId);
+        if(rows == 0){
+            throw new RuntimeException("can't delete this fee");
+        }
+    }
+    public double getFee(long driverId , long lotId){
+        String query = "SELECT fee FROM fees WHERE driver_id = ? AND lot_id = ?";
+        return jdbcTemplate.queryForObject(query, Double.class, driverId , lotId);
+    }
+    public boolean existsFee(long driverId, long lotId) {
+        String query = "SELECT COUNT(*) FROM fees WHERE driver_id = ? AND lot_id = ?";
+        Integer count = jdbcTemplate.queryForObject(query, Integer.class, driverId, lotId);
+        return count != null && count > 0;
+    }
+}
